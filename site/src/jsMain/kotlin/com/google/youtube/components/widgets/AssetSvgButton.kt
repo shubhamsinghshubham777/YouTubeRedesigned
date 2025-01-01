dfe0cf7733c39e9ea2364b8492dcf762017c032c
Shubham Singh <shubhamsinghshubham777@gmail.com>
2025-01-02 00:09:52 +0530

    Upgrade AssetSvgButton to also act as a Chip

--- site/src/jsMain/kotlin/com/google/youtube/components/widgets/AssetSvgButton.kt
+++ site/src/jsMain/kotlin/com/google/youtube/components/widgets/AssetSvgButton.kt
@@ -7,6 +7,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import com.google.youtube.utils.AnimatedVisibility
+import com.google.youtube.utils.Assets
 import com.google.youtube.utils.MouseEventState
 import com.google.youtube.utils.PaddingValues
 import com.google.youtube.utils.Styles
@@ -18,13 +20,14 @@ import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.UserSelect
 import com.varabyte.kobweb.compose.dom.disposableRef
-import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.background
+import com.varabyte.kobweb.compose.ui.modifiers.border
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.cursor
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
@@ -33,8 +36,13 @@ import com.varabyte.kobweb.compose.ui.modifiers.onClick
 import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.translateY
 import com.varabyte.kobweb.compose.ui.modifiers.userSelect
+import com.varabyte.kobweb.compose.ui.thenIf
 import com.varabyte.kobweb.silk.theme.shapes.Rect
 import com.varabyte.kobweb.silk.theme.shapes.clip
+import org.jetbrains.compose.web.css.CSSLengthOrPercentageValue
+import org.jetbrains.compose.web.css.LineStyle
+import org.jetbrains.compose.web.css.marginLeft
+import org.jetbrains.compose.web.css.marginRight
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Text
 
@@ -42,41 +50,65 @@ import org.jetbrains.compose.web.dom.Text
 fun AssetSvgButton(
     onClick: () -> Unit,
     id: String,
-    modifier: Modifier = Modifier,
+    type: AssetSvgButtonType = AssetSvgButtonType.Button,
     isSelected: Boolean = false,
-    path: String? = null,
+    isOutlined: Boolean = false,
+    startIconPath: String? = null,
+    endIconPath: String? = null,
+    iconSize: CSSLengthOrPercentageValue = if (type == AssetSvgButtonType.Button) 22.px else 24.px,
     text: String,
 ) {
-    val isSelectedState = updateTransition(isSelected)
+    val isSelectedTransition = updateTransition(isSelected)
+    val isOutlinedTransition = updateTransition(isOutlined)
+    val typeTransition = updateTransition(type)
+    val borderRadius = remember {
+        if (typeTransition.currentState == AssetSvgButtonType.Button) 24.px else 8.px
+    }
     var mouseEventState by remember { mutableStateOf(MouseEventState.Released) }
-    val contentPadding = remember(path) {
-        if (path != null) ButtonDefaults.ButtonWithIconContentPadding
-        else ButtonDefaults.ContentPadding
+    val contentPadding = remember(startIconPath) {
+        when (typeTransition.currentState) {
+            AssetSvgButtonType.Button -> PaddingValues(
+                left = if (startIconPath != null) 16.px else 24.px,
+                top = 10.px,
+                right = if (endIconPath != null) 16.px else 24.px,
+                bottom = 10.px,
+            )
+
+            else -> PaddingValues(
+                left = if (startIconPath != null) 12.px else 16.px,
+                top = 6.px,
+                right = if (endIconPath != null) 12.px else 16.px,
+                bottom = 6.px,
+            )
+        }
     }
     val animatedContainerColor by animateColorAsState(
-        when {
-            isSelectedState.currentState && mouseEventState == MouseEventState.Pressed ->
-                Colors.White.copyf(alpha = Styles.Opacity.PRESSED_SELECTED)
+        if (isSelectedTransition.currentState)
+            when (mouseEventState) {
+                MouseEventState.Pressed -> Styles.WHITE.copyf(alpha = Styles.Opacity.PRESSED_SELECTED)
+                MouseEventState.Hovered -> Styles.WHITE.copyf(alpha = Styles.Opacity.HOVERED_SELECTED)
+                MouseEventState.Released -> Styles.WHITE
+            }.toComposeColor()
+        else
+            when (mouseEventState) {
+                MouseEventState.Pressed -> Styles.WHITE.copyf(alpha = Styles.Opacity.PRESSED)
+                MouseEventState.Hovered -> Styles.WHITE.copyf(alpha = Styles.Opacity.HOVERED)
+                MouseEventState.Released -> {
+                    when {
+                        isOutlinedTransition.currentState -> Colors.Transparent
 
-            isSelectedState.currentState && mouseEventState == MouseEventState.Hovered ->
-                Colors.White.copyf(alpha = Styles.Opacity.HOVERED_SELECTED)
+                        typeTransition.currentState != AssetSvgButtonType.Button ->
+                            Styles.WHITE.copyf(alpha = Styles.Opacity.HOVERED)
 
-            isSelectedState.currentState && mouseEventState == MouseEventState.Released ->
-                Colors.White
-
-            !isSelectedState.currentState && mouseEventState == MouseEventState.Pressed ->
-                Colors.White.copyf(alpha = Styles.Opacity.PRESSED)
-
-            !isSelectedState.currentState && mouseEventState == MouseEventState.Hovered ->
-                Colors.White.copyf(alpha = Styles.Opacity.HOVERED)
-
-            else -> Colors.White.copyf(alpha = 0.05f)
-        }.toComposeColor()
+                        else -> Styles.WHITE.copyf(alpha = 0.05f)
+                    }
+                }
+            }.toComposeColor()
     )
     val animatedContentColor by animateColorAsState(
         when {
-            isSelectedState.currentState -> Colors.Black
-            else -> Colors.White
+            isSelectedTransition.currentState -> Colors.Black
+            else -> Styles.WHITE
         }.toComposeColor()
     )
 
@@ -91,7 +123,14 @@ fun AssetSvgButton(
         },
         modifier = Modifier
             .background(animatedContainerColor.toKobwebColor())
-            .clip(Rect(24.px))
+            .clip(Rect(borderRadius))
+            .then(
+                if (isOutlinedTransition.currentState) {
+                    Modifier
+                        .borderRadius(borderRadius)
+                        .border(1.px, LineStyle.Solid, Styles.WHITE.copyf(alpha = 0.2f))
+                } else Modifier
+            )
             .cursor(Cursor.Pointer)
             .padding(
                 left = contentPadding.left,
@@ -101,31 +140,56 @@ fun AssetSvgButton(
             )
             .onClick { onClick() }
             .color(animatedContentColor.toKobwebColor())
-            .fontSize(17.px)
+            .fontSize(if (typeTransition.currentState == AssetSvgButtonType.Button) 16.px else 14.px)
             .fontWeight(FontWeight.Medium)
-            .userSelect(UserSelect.None)
-            .then(modifier),
-        horizontalArrangement = Arrangement.spacedBy(8.px),
+            .userSelect(UserSelect.None),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        path?.let { safeIconRef ->
-            AssetSvg(
-                id = "asset_svg_button_$id",
-                path = safeIconRef,
-                size = 22.px,
-                primaryColor = animatedContentColor.toKobwebColor(),
-            )
+        val iconPath = when (typeTransition.currentState) {
+            AssetSvgButtonType.FilterChip -> startIconPath ?: Assets.Paths.CHECK
+            else -> startIconPath
         }
-        Box(modifier = Modifier.translateY(1.px)) { Text(text) }
+
+        iconPath?.let { safeIconPath ->
+            when (typeTransition.currentState) {
+                AssetSvgButtonType.FilterChip -> AnimatedVisibility(isSelectedTransition.currentState) {
+                    AssetSvg(
+                        id = "asset_svg_button_start_$id",
+                        path = safeIconPath,
+                        size = iconSize,
+                        primaryColor = animatedContentColor.toKobwebColor(),
+                    ) { marginRight(8.px) }
+                }
+
+                else -> AssetSvg(
+                    id = "asset_svg_button_start_$id",
+                    path = safeIconPath,
+                    size = iconSize,
+                    primaryColor = animatedContentColor.toKobwebColor(),
+                ) { marginRight(8.px) }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .translateY(1.px)
+                .thenIf(
+                    typeTransition.currentState != AssetSvgButtonType.Button,
+                    Modifier.padding(topBottom = 4.px),
+                )
+        ) { Text(text) }
+
+        if (typeTransition.currentState != AssetSvgButtonType.FilterChip) {
+            endIconPath?.let { safeIconRef ->
+                AssetSvg(
+                    id = "asset_svg_button_end_$id",
+                    path = safeIconRef,
+                    size = iconSize,
+                    primaryColor = animatedContentColor.toKobwebColor(),
+                ) { marginLeft(8.px) }
+            }
+        }
     }
 }
 
-object ButtonDefaults {
-    val ContentPadding: PaddingValues = PaddingValues(leftRight = 24.px, topBottom = 10.px)
-    val ButtonWithIconContentPadding: PaddingValues = PaddingValues(
-        left = 16.px,
-        top = 10.px,
-        right = 24.px,
-        bottom = 10.px,
-    )
-}
+enum class AssetSvgButtonType { Button, SelectableChip, FilterChip }
